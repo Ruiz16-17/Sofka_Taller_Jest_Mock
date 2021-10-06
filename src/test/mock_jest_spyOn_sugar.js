b19af9cee44a383362c4801b2a0d124e3447aa2c
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-10-05 21:31:11 -0500

    Finished

--- src/test/mock_jest_spyOn_sugar.js
+++ src/test/mock_jest_spyOn_sugar.js
@@ -1,1 +1,16 @@
+import * as app from "../component/example";
+import * as mathExample from "../component/mathExample";
 
+test("Calls mathExample.add", () => {
+    const originalAdd = mathExample.add;
+    mathExample.add = jest.fn(originalAdd);
+    expect(app.doAdd(1,2)).toEqual(3);
+    expect(mathExample.add).toHaveBeenCalledWith(1,2);
+
+    mathExample.add.mockImplementation(() => "mock");
+    expect(app.doAdd(1,2)).toEqual("mock");
+    expect(mathExample.add).toHaveBeenCalledWith(1,2);
+    
+    mathExample.add = originalAdd;
+    expect(app.doAdd(1,2)).toEqual(3);
+});
